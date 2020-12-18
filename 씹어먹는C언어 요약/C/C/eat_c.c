6ddb441fb54df78ffbdf7bdf0394d69db2ed5630
minteiro <dkdlel4763@naver.com>
2020-12-18 16:16:11 +0900

    씹어먹는c scanf 총정리

--- 씹어먹는C언어 요약/C/C/eat_c.c
+++ 씹어먹는C언어 요약/C/C/eat_c.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 int main() {
-	double celsius;
+	char ch;
 
-	printf("섭씨 온도를 화씨 온도로 바꿔주는 프로그램 입니다. \n");
-	printf("섭씨 온도를 입력해 주세요 : ");
-	scanf("%lf", &celsius);
+	short sh;
+	int i;
+	long lo;
 
-	printf("섭씨 %f 도는 화씨로 %f 도 입니다. \n", celsius, 9 * celsius / 5 + 32);
+	float fl;
+	double du;
+
+	printf("char 형 변수 입력 : ");
+	scanf("%c", &ch);
+
+	printf("short 형 변수 입력 : ");
+	scanf("%hd", &sh);
+	printf("int 형 변수 입력 : ");
+	scanf("%d", &i);
+	printf("long 형 변수 입력 : ");
+	scanf("%ld", &lo);
+
+	printf("float 형 변수 입력 : ");
+	scanf("%f", &fl);
+	printf("double 형 변수 입력 : ");
+	scanf("%lf", &du);
+
+	printf("char : %c, short : %d, int : %d ", ch, sh, i);
+	printf("long : %ld, float : %f, double : %f \n", lo, fl, du);
 
 	return 0;
+
 }
+
+
