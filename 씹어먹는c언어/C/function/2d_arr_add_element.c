441288d2673ef9ccf72b411c1750aaf222518fb9
minteiro <dkdlel4763@naver.com>
2020-12-29 15:48:54 +0900

    씹어먹는c_function_pointer

---
+++ 씹어먹는c언어/C/function/2d_arr_add_element.c
@@ -0,0 +1,60 @@
+int add1_element(int (*arr)[2], int row);
+int main()
+{
+	int arr[3][2];
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 2; j++)
+		{
+			scanf("%d", &arr[i][j]);
+		}
+	}
+	
+	add1_element(arr, 3);
+
+		
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 2; j++)
+		{
+			printf("arr[%d][%d] : %d \n", i, j, arr[i][j]);
+		}
+	}
+	return 0;
+}
+
+int add1_element(int (*arr)[2], int row) 
+{
+	int i, j;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < 2; j++)
+		{
+			arr[i][j]++;
+		}
+	}
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
