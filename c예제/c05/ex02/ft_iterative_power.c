58e7743a617c9753b61039fce805376b950cc0e9
minteiro <dkdlel4763@naver.com>
2021-01-04 23:42:25 +0900

    factorial_power_fibonacci

---
+++ c예제/c05/ex02/ft_iterative_power.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int ft_iterative_power(int nb, int power)
+{
+	int i;
+
+	if (power == 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	i = nb;
+	while(--power)
+		i *= nb;
+	return (i);
+}
+int main()
+{
+	printf("%d\n",ft_iterative_power(2, 6));
+}
