0e09a5c0b11fa62d57be7c06281bdcc4414dd7dc
minteiro <dkdlel4763@naver.com>
2021-07-26 16:19:37 +0900

    c02_re

--- 42Lapiscine/c02/ex07/re_strupcase.c
+++ 42Lapiscine/c02/ex07/re_strupcase.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
-
-
 char	*ft_strupcase(char *str)
 {
 	int i;
 
+	i = 0;
 	while (str[i])
 	{
-		char c = str[i];
-		if (c < 
+		if ('a' <= str[i] && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
+
+int main()
+{
+	char arr[] = "Hello123asadfgwqerwer";
+	*ft_strupcase(arr);
+	printf("%s \n", arr);
+
+	return 0;
+}
